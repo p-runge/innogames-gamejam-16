24e1c9fe5ff11c135daf59649597371544aca191
Marvin Demirkaya <m.demirkaya@progani.com>
2025-11-07 21:40:03 +0100

    build obstacle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { Canvas } from '@react-three/fiber'
-import Floor from './components/floor'
-import Player from './components/player'
+import { Canvas } from "@react-three/fiber";
+import Floor from "./components/floor";
+import Player from "./components/player";
+import Obstacle from "./components/obstacle";
 
 function App() {
-  return (
-    <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
-      <ambientLight intensity={Math.PI / 2} />
-      {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
-      <pointLight position={[0, 10, 5]} decay={0} intensity={Math.PI / 2} />
-      <Floor position={[0, -1, 1]} />
-      <Player position={[0, 0, 4]} />
-    </Canvas>
-  )
+    return (
+        <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
+            <ambientLight intensity={Math.PI / 2} />
+            {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
+            <pointLight
+                position={[0, 10, 5]}
+                decay={0}
+                intensity={Math.PI / 2}
+            />
+            <Floor position={[0, -1, 1]} />
+            <Player position={[0, 0, 4]} />
+            <Obstacle position={[0, 0, -4]} />
+        </Canvas>
+    );
 }
 
-export default App
+export default App;
--- src/components/obstacle.tsx
+++ src/components/obstacle.tsx
@@ -1,3 +1,4 @@
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
 
@@ -5,17 +6,17 @@ type Props = {
     position: [number, number, number];
 };
 
-export default function Box(props: Props) {
+export default function Obstacle(props: Props) {
     // This reference will give us direct access to the mesh
     const meshRef = useRef<THREE.Mesh>(null!);
 
     // Subscribe this component to the render-loop, rotate the mesh every frame
-    // useFrame((_state, delta) => (meshRef.current.rotation.x += delta))
+    useFrame((_state, delta) => (meshRef.current.position.z += delta));
 
     return (
         <mesh {...props} ref={meshRef}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color="orange" />
+            <boxGeometry args={[5, 1, 1]} />
+            <meshStandardMaterial color="red" />
         </mesh>
     );
 }
