d921eda855a1c125070a1dc14c01211a23b1ae7c
Marvin Demirkaya <m.demirkaya@progani.com>
2025-11-07 23:56:46 +0100

    add dynamic height for progress bar

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -1,10 +1,21 @@
 import { Html } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useState } from "react";
 
 export default function ProgressBar() {
+  const [timeprogress, setTimeProgress] = useState(0);
+
+  useFrame((state) => {
+    setTimeProgress(state.clock.getElapsedTime());
+  });
+
   return (
     <Html position={[2, 1, 0]} center>
       <div className="bg-white rounded shadow overflow-hidden h-[400px] justify-end w-12 flex flex-col relative">
-        <div className="bg-green-700 w-full h-1/2 relative">
+        <div
+          style={{ height: timeprogress }}
+          className="bg-green-700 w-full h-1/2 relative"
+        >
           <div className="absolute">Player</div>
         </div>
         <div className="absolute top-0">Zielfahne</div>
