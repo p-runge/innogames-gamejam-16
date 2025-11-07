1ac1da7c0448237f7e79ef7654f1b699f742dace
Marvin Demirkaya <m.demirkaya@progani.com>
2025-11-07 23:12:24 +0100

    build progress bar

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -2,9 +2,12 @@ import { Html } from "@react-three/drei";
 
 export default function ProgressBar() {
   return (
-    <Html position={[0, 1.2, 0]} center>
-      <div className="bg-white px-2 py-1 rounded shadow text-sm">
-        3D Box Label
+    <Html position={[2, 1, 0]} center>
+      <div className="bg-white rounded shadow overflow-hidden h-[400px] justify-end w-12 flex flex-col relative">
+        <div className="bg-green-700 w-full h-1/2 relative">
+          <div className="absolute">Player</div>
+        </div>
+        <div className="absolute top-0">Zielfahne</div>
       </div>
     </Html>
   );
