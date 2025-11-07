86ad198168eeb005fb005e9d8f3c2652e0c93d64
Philipp Runge <p.runge@progani.com>
2025-11-07 21:46:38 +0100

    fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,7 @@ function App() {
     <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
       <ambientLight intensity={Math.PI / 2} />
       {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
-      <pointLight
-        position={[0, 10, 5]}
-        decay={0}
-        intensity={Math.PI / 2}
-      />
+      <pointLight position={[0, 10, 5]} decay={0} intensity={Math.PI / 2} />
       <Floor position={[0, -1, 1]} />
       <Player position={[0, 0, 4]} />
       <Obstacle position={[0, 0, -4]} />
--- src/components/obstacle.tsx
+++ src/components/obstacle.tsx
@@ -3,20 +3,20 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 type Props = {
-    position: [number, number, number];
+  position: [number, number, number];
 };
 
 export default function Obstacle(props: Props) {
-    // This reference will give us direct access to the mesh
-    const meshRef = useRef<THREE.Mesh>(null!);
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef<THREE.Mesh>(null!);
 
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((_state, delta) => (meshRef.current.position.z += delta));
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((_state, delta) => (meshRef.current.position.z += delta));
 
-    return (
-        <mesh {...props} ref={meshRef}>
-            <boxGeometry args={[5, 1, 1]} />
-            <meshStandardMaterial color="red" />
-        </mesh>
-    );
+  return (
+    <mesh {...props} ref={meshRef}>
+      <boxGeometry args={[5, 1, 1]} />
+      <meshStandardMaterial color="red" />
+    </mesh>
+  );
 }
