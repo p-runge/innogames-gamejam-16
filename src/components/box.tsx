f8b07ef01bbe7fe61a4e45b52ad3f439b43dc4a5
Philipp Runge <p.runge@progani.com>
2025-11-07 20:40:41 +0100

    set up react three fiber

---
+++ src/components/box.tsx
@@ -0,0 +1,26 @@
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import * as THREE from 'three'
+
+type Props = {
+  position?: [number, number, number]
+}
+
+export default function Box(props: Props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef<THREE.Mesh>(null!)
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((_state, delta) => (meshRef.current.rotation.x += delta))
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+    // scale={1}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
