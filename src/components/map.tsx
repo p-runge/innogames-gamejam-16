bd2f0b8f93c2863c723b6084785cee166939f5f2
Philipp Runge <p.runge@progani.com>
2025-11-07 22:28:22 +0100

    render only visible obstacles in the map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,23 +1,35 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import type { Mesh } from "three";
 import Obstacle from "./obstacle";
 
 export default function Map() {
   const obstaclesZ: number[] = [
-    0,
+    -2,
     -6,
     -8,
   ];
   // This reference will give us direct access to the mesh
   const meshRef = useRef<Mesh>(null!);
 
+  const [visibleObstacles, setVisibleObstacles] = useState<number[]>([]);
+
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((_state, delta) => (meshRef.current.position.z += delta));
+  useFrame((_state, delta) => {
+    meshRef.current.position.z += delta;
+    setVisibleObstacles(
+      obstaclesZ.filter(z => (
+        // only show obstacles that are near the player
+        z + meshRef.current.position.z > -4 &&
+        // don't show obstacles that are out of view behind the player
+        z + meshRef.current.position.z < 7
+      ))
+    );
+  });
 
   return (
     <mesh ref={meshRef}>
-      {obstaclesZ.map((z, index) => (
+      {visibleObstacles.map((z, index) => (
         <Obstacle key={index} position={[0, 0, z]} />
       ))}
     </mesh>
