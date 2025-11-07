b3ed835d4de199d3da88705064e952eccc131d7d
Philipp Runge <p.runge@progani.com>
2025-11-08 00:03:48 +0100

    make progress bar fill within a minute

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -2,8 +2,12 @@ import { Html } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useState } from "react";
 
+const STAGE_DURATION = 60;
+
 export default function ProgressBar() {
   const [timeprogress, setTimeProgress] = useState(0);
+
+  const progress = Math.min(timeprogress / STAGE_DURATION * 100, 100)
 
   useFrame((state) => {
     setTimeProgress(state.clock.getElapsedTime());
@@ -13,7 +17,7 @@ export default function ProgressBar() {
     <Html position={[2, 1, 0]} center>
       <div className="bg-white rounded shadow overflow-hidden h-[400px] justify-end w-12 flex flex-col relative">
         <div
-          style={{ height: timeprogress }}
+          style={{ height: `${progress}%` }}
           className="bg-green-700 w-full h-1/2 relative"
         >
           <div className="absolute">Player</div>
