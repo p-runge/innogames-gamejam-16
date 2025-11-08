74fa9e87998e4fd69510c47ba8cb4f9f92449a3d
Marvin Demirkaya <m.demirkaya@progani.com>
2025-11-08 12:11:03 +0100

    add starting screen

--- src/components/scene-manager.tsx
+++ src/components/scene-manager.tsx
@@ -1,27 +1,28 @@
-import { useEffect } from 'react'
-import GameScene from '../scenes/game-scene'
-import { useGame } from '../stores/game'
-import { useScene, type SceneType } from '../stores/scene'
+import { useEffect } from "react";
+import GameScene from "../scenes/game-scene";
+import { useGame } from "../stores/game";
+import { useScene, type SceneType } from "../stores/scene";
+import MenuScene from "../scenes/menu-scene";
 
 export function SceneManager() {
-  const currentScene = useScene((s) => s.currentScene)
-  const setScene = useScene((s) => s.setScene)
+  const currentScene = useScene((s) => s.currentScene);
+  const setScene = useScene((s) => s.setScene);
 
   // handle death
-  const health = useGame((s) => s.health)
+  const health = useGame((s) => s.health);
   useEffect(() => {
     if (health <= 0) {
-      setScene('end')
+      setScene("end");
     }
-  }, [health, setScene])
+  }, [health, setScene]);
 
-  const Scene = sceneMap[currentScene]
+  const Scene = sceneMap[currentScene];
 
-  return <Scene />
+  return <Scene />;
 }
 
 const sceneMap: Record<SceneType, React.FC> = {
-  menu: () => <div>Menu Scene</div>,
+  menu: MenuScene,
   game: GameScene,
   end: () => <div>End Scene</div>,
-}
+};
---
+++ src/scenes/menu-scene.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useScene } from "../stores/scene";
+
+export default function MenuScene() {
+  const setScene = useScene((s) => s.setScene);
+  useEffect(() => {
+    const handleKeyDown = () => {
+      setScene("game");
+    };
+    window.addEventListener("keydown", handleKeyDown);
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-around items-center h-full font-extrabold">
+      <div className="text-4xl">GameName</div>
+      <button className="bg-red-500 text-white rounded-lg text-2xl p-2 px-4 font-bold">
+        Press any key to start your journey
+      </button>
+    </div>
+  );
+}
