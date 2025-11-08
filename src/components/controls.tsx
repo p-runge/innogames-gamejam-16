e2dbeaa19c33bc4a0d4375ccde3072ec498cb71c
Philipp Runge <p.runge@progani.com>
2025-11-08 11:01:00 +0100

    add controls

---
+++ src/components/controls.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import { useGame } from "../stores/game";
+
+export default function Controls() {
+  const [keysPressed, setKeysPressed] = useState<Set<string>>(new Set());
+  const playerPosition = useGame((state) => state.playerPosition);
+  const setPlayerPosition = useGame((state) => state.setPlayerPosition);
+
+  // handle player movement based on keys pressed
+  useEffect(() => {
+    console.log("Keys pressed:", keysPressed);
+    if (keysPressed.has("ArrowLeft") && !keysPressed.has("ArrowRight")) {
+      // Move player left
+      const playerIsInLeftPosition = playerPosition[0] === -1.5;
+      if (playerIsInLeftPosition) {
+        return;
+      }
+      setPlayerPosition(([
+        -1.5,
+        playerPosition[1],
+        playerPosition[2],
+      ]));
+    }
+    if (keysPressed.has("ArrowRight") && !keysPressed.has("ArrowLeft")) {
+      // Move player right
+      const playerIsInRightPosition = playerPosition[0] === 1.5;
+      if (playerIsInRightPosition) {
+        return;
+      }
+      setPlayerPosition(([
+        1.5,
+        playerPosition[1],
+        playerPosition[2],
+      ]));
+    }
+    if ((
+      !keysPressed.has("ArrowLeft") && !keysPressed.has("ArrowRight")
+    ) || (
+        keysPressed.has("ArrowLeft") && keysPressed.has("ArrowRight")
+      )) {
+      // Center player
+      const playerIsInCenterPosition = playerPosition[0] === 0;
+      if (playerIsInCenterPosition) {
+        return;
+      }
+      setPlayerPosition(([
+        0,
+        playerPosition[1],
+        playerPosition[2],
+      ]));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [keysPressed]);
+
+  // set up event listeners
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          if (!keysPressed.has("ArrowLeft")) {
+            setKeysPressed((prev) => new Set(prev).add("ArrowLeft"));
+          }
+          break;
+        case "ArrowRight":
+          if (!keysPressed.has("ArrowRight")) {
+            setKeysPressed((prev) => new Set(prev).add("ArrowRight"));
+          }
+          break;
+        default:
+          break;
+      }
+    }
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setKeysPressed((prev) => {
+        const next = new Set(prev);
+        if (prev.has(event.key)) {
+          next.delete(event.key);
+        }
+        return next;
+      });
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [keysPressed]);
+
+  return null;
+}
--- src/scenes/game-scene.tsx
+++ src/scenes/game-scene.tsx
@@ -6,17 +6,22 @@ import Map from "../components/map";
 import Player from "../components/player";
 import PowerUpUI from "../components/power-up-ui";
 import ProgressBar from "../components/progress-bar";
+import Controls from "../components/controls";
 
 export default function GameScene() {
   return (
     <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
+      <Controls />
+
       <ambientLight intensity={Math.PI / 2} />
       {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
       <pointLight position={[0, 10, 5]} decay={0} intensity={Math.PI / 2} />
+
       <Floor position={[0, -1, 1]} />
       <Player />
       <Map />
       <God />
+
       <ProgressBar />
       <HealthBar />
       <PowerUpUI />
