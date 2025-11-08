c12f3b1a11dee29b346c4c9940fc3475c08db3aa
Philipp Runge <p.runge@progani.com>
2025-11-08 01:12:35 +0100

    trigger end scene on death

--- src/components/scene-manager.tsx
+++ src/components/scene-manager.tsx
@@ -1,8 +1,16 @@
 import GameScene from '../scenes/game-scene'
+import { useGame } from '../store'
 import { useScene, type SceneType } from '../stores/scene'
 
 export function SceneManager() {
   const currentScene = useScene((s) => s.currentScene)
+  const setScene = useScene((s) => s.setScene)
+
+  // handle death
+  const health = useGame((s) => s.health)
+  if (health <= 0) {
+    setScene('end')
+  }
 
   const SceneComponent = sceneMap[currentScene]
 
